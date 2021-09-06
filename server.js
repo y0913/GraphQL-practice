8a5fcac69c7cc7dbcdf5f11cc6c63b531c209ae1
y0913 <yohei19910825@gmail.com>
2021-09-07 00:52:20 +0900

    update: パッケージを更新

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql')
 
 const app = express()
+
+// GraphQL呼び出し
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}))
 
 app.listen(4000, () => {
     console.log('listening port:3000')
